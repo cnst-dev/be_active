5359b4580d6216f2277d4623ba4a883cae9c99ce
Konstantin Khokhlov <sqb@mail.ru>
2017-06-07 16:51:07 +0400

    Started

---
+++ BeActive/BeActive WatchKit Extension/StartInterfaceController.swift
@@ -0,0 +1,12 @@
+//
+//  StartInterfaceController.swift
+//  BeActive
+//
+//  Created by Konstantin Khokhlov on 07.06.17.
+//  Copyright © 2017 Konstantin Khokhlov. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class StartInterfaceController: WKInterfaceController {}
---
+++ BeActive/BeActive/MainViewController.swift
@@ -0,0 +1,11 @@
+//
+//  MainViewController.swift
+//  BeActive
+//
+//  Created by Konstantin Khokhlov on 07.06.17.
+//  Copyright © 2017 Konstantin Khokhlov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {}
