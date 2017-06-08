ca9e05214ff2e5a8379e3e07bb5b4dcade7ed172
Konstantin Khokhlov <sqb@mail.ru>
2017-06-08 11:15:18 +0400

    StartInterface added

--- BeActive/BeActive WatchKit Extension/StartInterfaceController.swift
+++ BeActive/BeActive WatchKit Extension/StartInterfaceController.swift
@@ -8,5 +8,56 @@
 
 import WatchKit
 import Foundation
+import HealthKit
 
-class StartInterfaceController: WKInterfaceController {}
+class StartInterfaceController: WKInterfaceController {
+
+    // MARK: - Outlets
+    @IBOutlet private var activityPicker: WKInterfacePicker!
+
+    // MARK: - Properties
+    private let activities: [(name: String, type: HKWorkoutActivityType)] = [
+        ("Strength Training", .functionalStrengthTraining),
+        ("Yoga", .yoga),
+        ("Running", .running),
+        ("Meditation", .mindAndBody),
+        ("Cycling", .cycling)
+    ]
+
+    var currentActivity = HKWorkoutActivityType.functionalStrengthTraining
+
+    // MARK: - WKInterfaceController
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+
+        activityPicker.setItems(makePickerItems(from: activities))
+    }
+
+    // MARK: - Methods
+    /// Makes a picker items array from an activity dictionary.
+    ///
+    /// - Parameter dictionary: An activity dictionary.
+    /// - Returns: A picker items array.
+    func makePickerItems(from activities: [(name: String, type: HKWorkoutActivityType)]) -> [WKPickerItem] {
+        var activityItems = [WKPickerItem]()
+
+        for activity in activities {
+            let item = WKPickerItem()
+            item.title = activity.name
+            activityItems.append(item)
+        }
+        return activityItems
+    }
+
+    // MARK: - Actions
+    /// Sets the current activity.
+    ///
+    /// - Parameter value: A value of the selected line.
+    @IBAction private func activityPickerChanged(_ value: Int) {
+        currentActivity = activities[value].type
+    }
+
+    @IBAction func startButtonPressed() {
+        print(currentActivity.rawValue)
+    }
+}
